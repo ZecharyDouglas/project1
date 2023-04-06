b6b84f576cf4f93d663e19ae39a49f591e004017
ZecharyDouglas <Zechary.Douglas@cix.csi.cuny.edu>
2023-04-06 14:45:13 -0400

    Final commit that fixes parser Error

--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -1,16 +1,18 @@
-import { render, screen } from "@testing-library/react";
-import { describe, expect, it, vi } from "vitest";
-import React from "react";
-import App from "../App";
-import Navbar from "./Navbar";
-import Date from "./Date";
-import TestRenderer from "react-test-renderer";
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import React from 'react';
+import App from '../App';
+import Navbar from './Navbar';
 
-describe("test for the value of pieces of states", async () => {
-  it("testing the value of loadStatus", () => {
+describe('test for the value of pieces of states', async () => {
+  it('testing the value of loadStatus', () => {
     expect(Navbar.loadStatus).toBeFalsy();
   });
-  it.fails("testing if the rates in Navbar are not defined", () => {
+  it.fails('testing if the rates in Navbar are not defined', () => {
     expect(Navbar.rates).toBeDefined();
+  });
+  it('A button calle Conversions should be visible to the user ', async () => {
+    render(<Navbar />);
+    expect(screen.getByRole('button', { name: /Conversions/i })).toBeVisible();
   });
 });
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,30 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client " />
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.js',
+  },
+  esbuild: { loader: 'jsx', include: /src\/.*\.jsx?$/, exclude: [] },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: 'load-js-files-as-jsx',
+          setup(build) {
+            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => {
+              return {
+                loader: 'jsx',
+                contents: await fs.readFile(args.path, 'utf8'),
+              };
+            });
+          },
+        },
+      ],
+    },
+  },
 });
